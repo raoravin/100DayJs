252044858277024825454eb25d7cb0c337eb08d0
Ravin Yadav <ravinyadav973@gmail.com>
2024-03-19 20:29:52 +0530

    comparison operator

---
+++ 19mar comparison/index.js
@@ -0,0 +1,74 @@
+// ******************Comparison Operators *********************
+// Comparison operators are used in logical statements to determine equality or difference between variables or values.
+
+
+
+
+// let a = 5;
+// let b = "5";
+
+//  == vs ===
+
+// console.log(a==b);
+// console.log(a===b);
+
+
+// != vs !==
+
+// console.log(a!=b);
+// console.log(a!==b);
+
+
+// > and <
+
+// console.log(a < b);
+// console.log(a > b);
+
+
+// <= and >=
+
+// console.log(a <= b);
+// console.log(a >=b);
+
+
+
+
+
+
+// ************ Logic Operator *************
+
+// 1.AND Operator
+
+// let x = 10;
+// let y = 5;
+
+// console.log(x > 10 && y < 10);
+
+
+
+// 2.OR
+
+// console.log(x > 10 || y < 10);
+
+
+// 3.NOT
+
+// console.log(x != 10);
+
+
+
+
+
+
+
+// ************ Conditional (Ternary) Operator *********
+
+// variablename = (condition) ? value1:value2 
+
+// let age  = 10;
+// let voteable = (age < 18) ? "Too young":"Old enough";
+// console.log(voteable);
+
+
+
+
